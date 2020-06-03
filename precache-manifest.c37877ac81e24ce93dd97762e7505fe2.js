9ad24bcd4b8e221379203c40484d077e482cc832
Vladimir Pasechnik <vladimir.pasechnik@gmail.com>
2020-06-04 00:57:01 +0200

    Updates

---
+++ precache-manifest.c37877ac81e24ce93dd97762e7505fe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b5525e4359b836d83a08db63d25044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b32cc582fc4b530848e0",
+    "url": "/static/css/main.186e82f0.chunk.css"
+  },
+  {
+    "revision": "e790668a3a245a19ebbe",
+    "url": "/static/js/2.b1e006eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1e006eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32cc582fc4b530848e0",
+    "url": "/static/js/main.c47c2747.chunk.js"
+  },
+  {
+    "revision": "c566f7cbf1175950c4d3",
+    "url": "/static/js/runtime-main.ee2d8e30.js"
+  }
+]);
